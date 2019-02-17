ed5f83a32176cfd3afa3168ca2a77a09600d41f6
Juliana Dias Santos e Silva <juuh42dias@gmail.com>
2019-02-17 19:32:56 -0300

    Add cofig fallbacks to i18n

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Transervicos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.fallbacks = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
