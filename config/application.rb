dac6db23d78903637a6f947dbff30d1f4820b81c
Juliana Dias Santos e Silva <juuh42dias@gmail.com>
2019-02-17 20:28:51 -0300

    Fix i18n locales changing config from true to get default locale

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Transervicos
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.i18n.fallbacks = true
+    config.i18n.fallbacks = [I18n.default_locale]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
