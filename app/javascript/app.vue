e550f06f0d5506947f358eafd4df619f755d12fe
Juliana Dias Santos e Silva <juuh42dias@gmail.com>
2019-03-17 20:14:28 -0300

    Add npm build Refactor getJobs method

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home-job-list">
     <ul>
-      <li v-for="job in jobs">
+      <li v-for="job in jobs" :key="job.id">
         {{ job.title }}
       </li>
     </ul>
@@ -10,14 +10,19 @@
 
 <script>
 export default {
-  name: "home-job-list"
-  data: function () {
-    let response = await fetch('/jobs.json');
-    return {
-      response
-    }
-  }
+  name: "home-job-list",
+  data: getJobs()
 }
+
+function getJobs(){
+  fetch('/jobs.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+    });
+};
 </script>
 
 <style scoped>
